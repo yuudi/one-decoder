10991adecb7a992143bcc881df0b2d465823946f
yuudi <yuudi@users.noreply.github.com>
2025-07-18 05:45:01 +0000

    ➕ move plugin to npm

---
+++ src/plugins/memes/cherugo.ts
@@ -0,0 +1,28 @@
+import { Plugin } from '../../decoders/decorators';
+
+@Plugin({
+  id: 'cherugo',
+  name: '切噜语',
+  author: '@Ice9Coffee',
+  link: 'https://pcrbot.github.io/cherugo.js/',
+  needKey: false,
+})
+export class CherugoDecoder {
+  checkString(input: string): number {
+    if (input.startsWith('切噜～♪')) {
+      return 100;
+    }
+
+    return 0;
+  }
+
+  async decode(input: string): Promise<string> {
+    const { decode } = await import('cherugo'); // lazy load module
+    return decode(input);
+  }
+
+  async encode(input: string): Promise<string> {
+    const { encode } = await import('cherugo'); // lazy load module
+    return encode(input);
+  }
+}
